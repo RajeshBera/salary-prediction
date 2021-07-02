e4764d46cdf3ce67ee9c69df6e3586acc124a0ff
RajeshBera <81524523+RajeshBera@users.noreply.github.com>
2021-07-02 10:02:15 +0530

    Add files via upload

---
+++ simple_linear_regression.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import linear_model
+
+
+# In[2]:
+
+
+df = pd.read_csv('Salary_Data.csv')
+
+
+# In[3]:
+
+
+reg = linear_model.LinearRegression()
+reg.fit(df[['YearsExperience']],df.Salary)
+
+
+# In[4]:
+
+
+plt.scatter(df.YearsExperience,df.Salary,color='red')
+plt.plot(df[['YearsExperience']], reg.predict(df[['YearsExperience']]),color='green')
+plt.xlabel("Experience")
+plt.ylabel("Salary")
+plt.title("Salary predection with experience")
+
+
+# In[5]:
+
+
+reg.predict([[1]])
+
+
+# In[8]:
+
+
+reg.intercept_
+
+
+# In[9]:
+
+
+reg.coef_
+
+
+# In[31]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
